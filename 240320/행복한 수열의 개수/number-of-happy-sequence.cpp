d3423ed7f76e8d22792b5e246e7fd652cfa08a68
ComPhyPark <69303148+ComPhyPark@users.noreply.github.com>
2024-03-20 19:19:49 +0900

    Title: 행복한 수열의 개수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+int a[110][110];
+int main() {
+    int n,m,i,j,r=0;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        int cont_num=1;
+        for(j=1;j<n;j++){
+            if(a[i][j]==a[i][j-1])cont_num++;
+            else cont_num=1;
+            if(cont_num>=m){
+                r++;
+                break;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        int cont_num=1;
+        for(j=1;j<n;j++){
+            if(a[j][i]==a[j-1][i])cont_num++;
+            else cont_num=1;
+            if(cont_num>=m){
+                r++;
+                break;
+            }
+        }
+    }
+    printf("%d",r);
+    return 0;
+}
